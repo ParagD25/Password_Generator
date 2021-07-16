f9988ce45f673c12f9209693dfe629cb52fa935c
Parag <paragdurafe@outlook.com>
2021-07-16 19:50:59 +0530

    initial commit

---
+++ password_generator_gui/random_password.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+password_window=Tk()
+password_window.geometry('425x325')
+heading=Label(password_window,text='Create Random Password\n of any length',font='helvetica 24 bold')
+heading.place(x=0,y=50)
+
+password_window.mainloop()
